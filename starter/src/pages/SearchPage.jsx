edea0dc50889a25f1c01e69019e3db3b7658e028
Hossam Hassan <hossam.mohamed@thegroup.com.qa>
2022-08-12 01:45:23 +0200

    implement Routes

---
+++ starter/src/pages/SearchPage.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const SearchPage = (props) => {
+  return (
+    <div className="search-books">
+      <div className="search-books-bar">
+        <Link to="/" className="close-search">
+          Add a book
+        </Link>
+
+        <div className="search-books-input-wrapper">
+          <input type="text" placeholder="Search by title, author, or ISBN" />
+        </div>
+      </div>
+      <div className="search-books-results">
+        <ol className="books-grid"></ol>
+      </div>
+    </div>
+  );
+};
+
+SearchPage.propTypes = {};
+
+export default SearchPage;
